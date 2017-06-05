1470dc6898723e1d79dbc911bd715fb94ae88afc
ssyyjj88 <sunyujuan@syjdeMacBook-Pro.local>
2017-06-05 23:04:31 +0800

    no message

--- backend/views/post/_form.php
+++ backend/views/post/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use common\models\Poststatus;
+use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Post */
@@ -18,7 +20,52 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'tags')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'status')->textInput() ?>
+	<?php 
+	/*
+	 * 第一种方法：
+	$psObjs = Poststatus::find()->all();
+	$allStatus = ArrayHelper::map($psObjs, 'id', 'name');
+	
+	第二种方法：
+	$psObjs = Yii::$app->db->createCommand('select id,name from poststatus');
+	$allStatus = ArrayHelper::map($psObjs, 'id', 'name');
+	
+	第三种方法：
+	$allStatus = (new \yii\db\Query())
+	->select(['name','id'])
+	->from('poststatus')
+	->indexBy('id')
+// 	->all();
+	->column();
+// 	->scalar();
+//     ->count();
+	
+	第四种方法：
+	$allStatus= Poststatus::find()
+	->select(['name','id'])
+	->orderBy('position')
+	->indexBy('id')
+	->column();
+	
+	
+	
+    <?= $form->field($model, 'status')->dropDownList([1=>'草稿',2=>'已发布'],['prompt'=>'请选择状态']); ?>
+	<?= $form->field($model, 'status')->dropDownList($allStatus,['prompt'=>'请选择状态']); ?>
+
+	*/
+	
+// 	echo "<hr><pre>";
+// 	print_r($allStatus);
+// 	echo "</pre>";
+	
+// 	exit(0);
+	
+	?>
+ 	<?= $form->field($model, 'status')->dropDownList(Poststatus::find()
+	->select(['name','id'])
+	->orderBy('position')
+	->indexBy('id')
+	->column(),['prompt'=>'请选择状态']); ?>
 
     <?= $form->field($model, 'create_time')->textInput() ?>
 
